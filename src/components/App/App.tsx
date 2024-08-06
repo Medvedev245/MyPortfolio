4500d9009332881866853d8faa6ee5ad35a18028
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-08-06 10:36:49 +0200

    add header, footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import Footer from '../Footer/Footer.tsx';
+import Header from '../Header/Header.tsx';
 
 export const App: React.FC = () => {
   return (
