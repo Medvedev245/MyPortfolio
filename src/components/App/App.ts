ecd3826060e2e1cc0d2f57e420000a12ac76f59c
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-08-09 22:16:08 +0200

    add some styles for App

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,1 +1,14 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  width: 1200px;
+  text-align: center;
+
+  @media screen and (max-width: 1200px) {
+  }
+
+  @media screen and (max-width: 767px) {
+  }
+`;
+
 export {};
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from 'components/Footer/Footer.tsx';
 import Header from 'components/Header/Header.tsx';
 import Main from 'components/Main/Main.tsx';
+import { FormContainer } from './App';
 
 export const App: React.FC = () => {
   return (
-    <div>
+    <FormContainer>
       <Header />
       <Main />
       <Footer />
-    </div>
+    </FormContainer>
   );
 };
