6f20cf31cd2cadd2aec090ca843255cb344f5ace
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2025-09-14 10:56:46 +0200

    add translate to about block

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -7,31 +7,17 @@ import {
   Motivation,
   EducationWrap,
 } from './Education.styled';
+import { useTranslation } from 'react-i18next';
 
 const Education: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <EducationSection>
       <EducationWrap>
-        <FewWords>A FEW WORDS ABOUT ME.</FewWords>
-        <SpanAll>About</SpanAll>
-        <Motivation>
-          "Your determination and perseverance define your future. Never give
-          up!"
-        </Motivation>
-        <EducationDescr>
-          Hello everyone, my name is Igor. I’m from Kyiv, Ukraine. Currently, I
-          live and work at Vysočinské nemocnice in Humpolec, Czech Republic,
-          where I’ve been for the past 2 years. I graduated from Medical
-          University in 2012 and started my medical career shortly after.
-          However, in 2021, I developed an interest in single-page website
-          development. Realizing I needed more structured knowledge, I took a
-          course in HTML/CSS. What began as a hobby quickly became a passion and
-          guided my decision on my future tech stack. I then completed a
-          year-long Full-Stack Developer course from January 20, 2023, to
-          February 14, 2024, and will receive a certificate. At the moment, I
-          have more than a year of practice and have 8 projects in my portfolio,
-          both team and individual.
-        </EducationDescr>
+        <FewWords>{t('education.title')}</FewWords>
+        <SpanAll>{t('education.About')}</SpanAll>
+        <Motivation>{t('education.motivation')}</Motivation>
+        <EducationDescr>{t('education.description')}</EducationDescr>
       </EducationWrap>
     </EducationSection>
   );
